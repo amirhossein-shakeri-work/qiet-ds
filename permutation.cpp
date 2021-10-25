770eb71519da640de742ccb52670a0452a52ab26
Amirhossein Shakeri <58584189+sizata-siege@users.noreply.github.com>
2021-10-25 16:59:05 +0330

    implement permutation

--- permutation.cpp
+++ permutation.cpp
@@ -3,9 +3,33 @@
 
 using namespace std;
 
-int main () {
+void permutation(string, int, int);
+void print(string);
 
-    cout << "Foo";
-    
+int main()
+{
+
+    permutation("ABC", 0, 3);
+
     return 0;
+}
+
+void permutation(string a, int start, int len)
+{
+    if (start == len - 1) 
+        return print(a);
+
+    for (int i = start; i < len; i++) {
+        swap(a[start], a[i]);
+        permutation(a, start + 1, len);
+        swap(a[start], a[i]);
+    }
+}
+
+void print(string a) {
+    cout << a << endl;
+    // cout << "[";
+    // for (int i = 0; a[i]; i++)
+    //     cout << a[i] << ", ";
+    // cout << "]\n";
 }
